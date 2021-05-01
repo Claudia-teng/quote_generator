807c8fb8f096bb7c7e3452c2a63e6b308a6f20fd
Claudia <sydeng102@gmail.com>
2021-05-01 20:22:38 +0800

    add loader

--- script.js
+++ script.js
@@ -4,12 +4,26 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
+
+// Loading Spinner Shown
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Remove Loading Spinner
+function complete() {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+}
 
 // Show new Quote
 
 function newQuote() {
+  loading();
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
   // Check if Author is blank and replace it with unknown
@@ -26,19 +40,21 @@ function newQuote() {
     quoteText.classList.remove('long-quote');
   }
   quoteText.textContent = quote.text;
+  complete();
 }
 
 // Get quotes from api
 async function getQuotes() {
-    const apiUrl = 'https://type.fit/api/quotes';
-    try {
-        const response = await fetch(apiUrl);
-        apiQuotes = await response.json();
-        console.log('apiQuotes', apiQuotes);
-        newQuote();
-    } catch (error) {
-        // Catch error here
-    }
+  loading();
+  const apiUrl = 'https://type.fit/api/quotes';
+  try {
+      const response = await fetch(apiUrl);
+      apiQuotes = await response.json();
+      console.log('apiQuotes', apiQuotes);
+      newQuote();
+  } catch (error) {
+      // Catch error here
+  }
 }
 
 
